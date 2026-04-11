78b8ed93677aeea15ac43d94c79fab161e9a1620
juakincruzz <joaquinjcl2004@gmail.com>
2026-04-11 13:05:30 +0200

    Añadido el mapa del ejercicio cuatro como catapults2026_lvl8.txt

--- P1/GVGAI-master/src/tracks/singlePlayer/Test.java
+++ P1/GVGAI-master/src/tracks/singlePlayer/Test.java
@@ -44,7 +44,7 @@ public class Test {
 
 		// Game and level to play
 		int gameIdx = 122;
-		int levelIdx = 6; // level names from 0 to 4 (game_lvlN.txt).
+		int levelIdx = 8; // level names from 0 to 4 (game_lvlN.txt).
 		String gameName = games[gameIdx][1];
 		String game = games[gameIdx][0];
 		String level1 = game.replace(gameName, gameName + "_lvl" + levelIdx);
@@ -55,11 +55,11 @@ public class Test {
 						// executed. null if not to save.
 
 		// 1. This starts a game, in a level, played by a human.
-		// ArcadeMachine.playOneGame(game, level1, recordActionsFile, seed);
+		ArcadeMachine.playOneGame(game, level1, recordActionsFile, seed);
 
 		// 2. This plays a game in a level by the controller.
 		//ArcadeMachine.runOneGame(game, level1, visuals, sampleRHEAController, recordActionsFile, seed, 0);
-		ArcadeMachine.runOneGame(game, level1, visuals, agentName, recordActionsFile, seed, 0) ;
+		//ArcadeMachine.runOneGame(game, level1, visuals, agentName, recordActionsFile, seed, 0) ;
 
 		// 3. This replays a game from an action file previously recorded
 	//	 String readActionsFile = recordActionsFile;
